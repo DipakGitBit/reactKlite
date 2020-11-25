25f878e96048790dc83d6f3ac48671901f084184
dipakvit <dipakpaul199@gmail.com>
2020-11-25 10:03:58 +0530

    klite website

---
+++ src/Home.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+//import "./slider.scss";
+
+/*function Home(){
+    let Arr = [1, 2, 3, 4, 5]; 
+    const [x,setX]=useState(0);
+
+    const goLeft=()=>{
+        setX(x+100);
+    };
+    const goRight=()=>{
+        setX(x-100);
+    };
+return (
+        <div className="slider">
+    {sliderArr.map((item,index)=>{
+            return(
+                <div key={index} className="slide" style={{transform:`translate(${x}%)`}}>
+                    {item}
+                </div> 
+            );
+        })}
+<button id="goLeft" onClick={goLeft}>left</button>
+<button id="goRight" onClick={goRight}>right</button>
+
+    </div>
+    );
+}*/
+const Home=()=>{
+    return (
+    <>
+    <h1>welcome Home</h1>
+    </>
+    );
+};
+
+export default Home; 
---
+++ src/Slider.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Slider=()=>{
+    return(
+        <>
+        <div className="slider">
+            <p>sider</p>
+        </div>
+
+        </>
+    );
+};
+export default Slider;
